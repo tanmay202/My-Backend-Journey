ec28568fb62787167bc894f097f2e6dd3145ca3a
Tanmay Biswas <tanmaybiswastb202@gmail.com>
2026-01-06 21:40:50 +0530

    Create Item model class with basic fields

--- projects/mysite/myapp/models.py
+++ projects/mysite/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    item_name=models.CharField()
+    item_desc=models.CharField()
+    item_price=models.FloatField()
+    item_quantity=models.IntegerField()
