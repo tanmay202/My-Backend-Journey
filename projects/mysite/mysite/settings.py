9c1c822b0ce9da1f6c85dbf45c39ea318bf9bbd6
Tanmay Biswas <tanmaybiswastb202@gmail.com>
2026-01-07 22:54:46 +0530

    Created superuser and Added some data through django admin panel

--- projects/mysite/mysite/settings.py
+++ projects/mysite/mysite/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'myapp.apps.MyappConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'myapp', 
+    'django.contrib.staticfiles',   
 ]
 
 MIDDLEWARE = [
