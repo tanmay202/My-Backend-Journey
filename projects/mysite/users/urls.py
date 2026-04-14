29dd83422126d983797f2caeba19c86f98964abd
Tanmay Biswas <tanmaybiswastb202@gmail.com>
2026-04-14 08:21:29 +0530

    Added login functionality

--- projects/mysite/users/urls.py
+++ projects/mysite/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .import views 
+from django.contrib.auth import views as loginView
 urlpatterns = [
-    path('register/',views.register)
+    path('register/',views.register),
+    path('login/',loginView.LoginView.as_view(template_name='users/login.html')),
 ]
