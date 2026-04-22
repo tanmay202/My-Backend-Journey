8b05cab85bf38e1f13e7a645a9a46d3e36347dd9
Tanmay Biswas <tanmaybiswastb202@gmail.com>
2026-04-22 23:56:30 +0530

    Fixing the syntax

--- projects/mysite/mysite/settings.py
+++ projects/mysite/mysite/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'myapp.middleware.LogRequestMiddleware'
+    'myapp.middleware.LogRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
