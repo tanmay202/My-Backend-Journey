54273c5545b5ba75f51f5d31f970ed840ae2d349
Tanmay Biswas <tanmaybiswastb202@gmail.com>
2026-04-23 22:05:59 +0530

    Added a TimeCountMiddleware

--- projects/mysite/myapp/middleware.py
+++ projects/mysite/myapp/middleware.py
@@ -8,3 +8,20 @@ class LogRequestMiddleware:
         
         print(f"[Middleware] response status:{response.status_code}")
         return response
+    
+import time
+
+class TimeCountMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start = time.time()
+
+        response = self.get_response(request)
+
+        duration = time.time() - start
+        print(f'[Middleware] took {duration:.4f} seconds')
+
+        return response
+        
--- projects/mysite/mysite/settings.py
+++ projects/mysite/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'myapp.middleware.TimeCountMiddleware',
     'myapp.middleware.LogRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
