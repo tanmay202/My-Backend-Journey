b17a6d8b87c281cbc8624179621e13411ed76a3d
Tanmay Biswas <tanmaybiswastb202@gmail.com>
2026-04-23 22:13:15 +0530

    Learned how to create IP blocking middleware

--- projects/mysite/myapp/middleware.py
+++ projects/mysite/myapp/middleware.py
@@ -24,4 +24,34 @@ class TimeCountMiddleware:
         print(f'[Middleware] took {duration:.4f} seconds')
 
         return response
+    
+class IPBlockerMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+        # List of blocked IPs
+        self.blocked_ips = [
+            '127.0.0.1',   # IPv4 localhost
+            '::1',         # IPv6 localhost
+        ]
+
+    def __call__(self, request):
+        ip = self.get_client_ip(request)
+
+        if ip in self.blocked_ips:
+            from django.http import HttpResponseForbidden
+            return HttpResponseForbidden("Access Denied 🚫")
+
+        return self.get_response(request)
+
+    def get_client_ip(self, request):
+        # Handle proxy headers (important in real-world apps)
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        return ip
         
